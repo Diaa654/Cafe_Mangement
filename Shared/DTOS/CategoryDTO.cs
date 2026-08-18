22ce4dabe00b7f8def6100fdaa470f5460533e16
ElHussien2004 <abotaleb2322004@gmail.com>
2026-08-18 16:07:58 -0700

    AddCategoryServic

---
+++ Shared/DTOS/CategoryDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOS
+{
+    public class CategoryDTO
+    {
+        public string Name {  get; set; }
+    }
+}
