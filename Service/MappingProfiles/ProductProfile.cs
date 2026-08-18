664a9d030390aa210093ca6fd897f045d723a43c
ElHussien2004 <abotaleb2322004@gmail.com>
2026-08-18 16:07:58 -0700

    AddCategoryServic

---
+++ Service/MappingProfiles/ProductProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Domain.Models;
+using Shared.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.MappingProfiles
+{
+    public class ProductProfile: Profile
+    {
+        public ProductProfile() {
+
+            CreateMap<AddProductDTO, Product>()
+                .ForMember(des => des.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(des => des.Price, opt => opt.MapFrom(src => src.Price))
+                .ForMember(des => des.CategoryId, opt => opt.MapFrom(src => src.CategoryId))
+                .ForMember(des=>des.Discount, opt => opt.MapFrom(src => src.Discount))
+                .ForMember(des=>des.IsAvailable, opt => opt.MapFrom(src => src.IsAvailable));
+        
+        
+        }
+    }
+}
---
+++ Shared/DTOS/GetAllProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOS
+{
+    public class GetAllProductDTO
+    {
+        public string Name { get; set; }
+        public decimal PriceAfterDiscount { get; set; }
+        public bool IsDiscount { get; set; }
+        public decimal Discount { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
