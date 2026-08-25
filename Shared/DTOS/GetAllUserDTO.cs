5929281e474af32a8189fcabac18d4f446a31077
Diaa654 <diaaemad156@gmail.com>
2026-08-25 16:03:10 +0300

    Add Migration and Edit Get Product service

--- Shared/DTOS/GetAllUserDTO.cs
+++ Shared/DTOS/GetAllUserDTO.cs
@@ -8,10 +8,11 @@ namespace Shared.DTOS
 {
     public class GetAllUserDTO
     {
+        public int Id { get; set; }
         public string FuName { get; set; } = default!;
 
         public string Email { get; set; }= default!;
-
+        public string Role { get; set; } = default!;
         public bool IsActive { get; set; }
     }
 }
